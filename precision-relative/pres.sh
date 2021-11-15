7cdf400e2af5d57dbdff8190b0ade83fe616f049
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2021-11-15 17:51:14 -0600

    Add variable BITWIDTH to pres.sh script

--- precision-relative/pres.sh
+++ precision-relative/pres.sh
@@ -5,17 +5,19 @@ make clean
 make kern_mul_v_our_mul
 make bitwise_mul_v_our_mul
 
+BITWIDTH=8
+
 printf "\n---------------------\n"
 printf "kern_mul v our_mul\n"
-./pres_kern_mul_v_our_mul.out 8 > pres_kern_mul_v_our_mul.log && 
+./pres_kern_mul_v_our_mul.out ${BITWIDTH} > pres_kern_mul_v_our_mul.log && 
 tail -3 pres_kern_mul_v_our_mul.log && 
 printf "\n---------------------\n"
 printf "bitwise_mul v our_mul\n"
-./pres_bitwise_mul_v_our_mul.out 8 > pres_bitwise_mul_v_our_mul.log && 
+./pres_bitwise_mul_v_our_mul.out ${BITWIDTH} > pres_bitwise_mul_v_our_mul.log && 
 tail -3 pres_bitwise_mul_v_our_mul.log && 
 printf "\n---------------------\n"
 printf "preparing graph ..."
-python3 graph_precision_relative.py --bitwidth=8 --infile1=pres_kern_mul_v_our_mul.log --infile2=pres_bitwise_mul_v_our_mul.log --outfile=pres_fig.png --op1=kern_mul --op2=bitwise_mul --op=our_mul 
+python3 graph_precision_relative.py --bitwidth=${BITWIDTH} --infile1=pres_kern_mul_v_our_mul.log --infile2=pres_bitwise_mul_v_our_mul.log --outfile=pres_fig.png --op1=kern_mul --op2=bitwise_mul --op=our_mul 
 printf " done.\n\n"
 
 
