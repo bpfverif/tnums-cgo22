441e0b44d8c7a0b6d454cb29744afbcf1758324f
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2021-11-16 17:47:38 -0600

    precision-relative: second precision comparison's graphing follows the first one

--- precision-relative/graph_precision_relative.py
+++ precision-relative/graph_precision_relative.py
@@ -11,6 +11,10 @@ palette = plt.get_cmap('Set1')
 mp.rcParams['figure.autolayout'] = True
 mp.rcParams['font.size'] = 12
 
+# change "bitwidth" here if needed (for display only)
+legend_label = "$\\bf{bitwidth:}$" + " " + "$\\bf{8}$"+ " " +"$\\bf{(exhaustive)}$" 
+
+# log base 2 CDF
 def log2cdf(data):
 	# y = P(imprecision < x)
 	data = np.log2(data)
@@ -23,59 +27,48 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--bitwidth", help="bitvector width", type=int, 
 		required=True)
-	parser.add_argument("--infile1", help="input file", type=str, 
+	parser.add_argument("--baseop", help="Base tnum op for comparison", type=str, 
 		required=True)
-	parser.add_argument("--infile2", help="input file", type=str, 
+	parser.add_argument("--op1", help="The tnum op that baseop is compared against", type=str, 
 		required=True)
-	parser.add_argument("--outfile", help="output file", type=str, 
+	parser.add_argument("--infile1", help="Log file corresponding to the comparison baseop-op1", type=str, 
 		required=True)
-	parser.add_argument("--op1", help="op1", type=str, 
+	parser.add_argument("--op2", help="The tnum op that baseop is compared against", type=str, 
 		required=True)
-	parser.add_argument("--op2", help="op2", type=str, 
+	parser.add_argument("--infile2", help="Log file corresponding to the comparison baseop-op2", type=str, 
 		required=True)
-	parser.add_argument("--op", help="op", type=str, 
+	parser.add_argument("--outfile", help="Output file name for figure", type=str, 
 		required=True)
 
 	args=parser.parse_args()
 
+	args.baseop = args.baseop.replace("_", "\\_")
 	args.op1 = args.op1.replace("_", "\\_")
-	args.op2 = args.op2.replace("_", "\\_")
-	args.op = args.op.replace("_", "\\_")
-
-	data1 = np.loadtxt(open(args.infile1).readlines()[:-3], delimiter=",", skiprows=1)
-	data2 = np.loadtxt(open(args.infile2).readlines()[:-3], delimiter=",", skiprows=1)
 
 	fig = plt.figure(1)
 	axis = fig.add_subplot(111)
 
-	legend_label = "$\\bf{bitwidth:}$" + " " + "$\\bf{8}$"+ " " +"$\\bf{(exhaustive)}$"
-
-	# labeling
+	# load data1, and plot it
+	data1 = np.loadtxt(open(args.infile1).readlines()[:-3], delimiter=",", skiprows=1)
 	data1_label = "$log_{{2}} \\frac{{\\vert{{}} {0} \\vert{{}} }}{{\\vert{{}} {{{1}}} \\vert{{}} }}$".format(
-		args.op1, args.op)
-	data2_label = "$log_{{2}} \\frac{{\\vert{{}} {0} \\vert{{}} }}{{\\vert{{}} {{{1}}} \\vert{{}} }}$".format(
-		args.op2, args.op)
-	x_label =\
-	"$log_{{2}} \\frac{{\\vert{{}} {0} \\vert{{}} }}{{\\vert{{}} {{{1}}} \\vert{{}} }}$".format(
-		args.op1, args.op)
-	x_label += "  OR   "
-	x_label +=\
-	"$log_{{2}} \\frac{{\\vert{{}} {0} \\vert{{}} }}{{\\vert{{}} {{{1}}} \\vert{{}} }}$".format(
-		args.op2, args.op)
-
-	# plot
-	x1, y1 = log2cdf(data1)           
-	x2, y2 = log2cdf(data2)           
+		args.op1, args.baseop)
+	x1, y1 = log2cdf(data1)
 	axis.plot(x1, y1, 
 		label = data1_label, marker='', linewidth=3, 
 		color = "black",
 		linestyle = "dotted", alpha = 1)
+
+	# load data2, and plot it
+	args.op2 = args.op2.replace("_", "\\_")
+	data2 = np.loadtxt(open(args.infile2).readlines()[:-3], delimiter=",", skiprows=1)
+	data2_label = "$log_{{2}} \\frac{{\\vert{{}} {0} \\vert{{}} }}{{\\vert{{}} {{{1}}} \\vert{{}} }}$".format(
+		args.op2, args.baseop)
+	x2, y2 = log2cdf(data2)           
 	axis.plot(x2, y2, 
 		label = data2_label, marker='', linewidth=3, 
 		color = "black",
 		linestyle = "solid", alpha=0.5)
 
-	# axis.set_xlabel(x_label, fontsize = 15)
 	axis.set_ylabel("probability (at or below value)")
 	axis.grid()
 	axis.legend(title=legend_label, prop={'size': 15})
--- precision-relative/pres.sh
+++ precision-relative/pres.sh
@@ -16,7 +16,7 @@ printf "bitwise_mul v our_mul\n"
 tail -3 pres_bitwise_mul_v_our_mul.log && 
 printf "\n---------------------\n"
 printf "preparing graph ..."
-python3 graph_precision_relative.py --bitwidth=${BITWIDTH} --infile1=pres_kern_mul_v_our_mul.log --infile2=pres_bitwise_mul_v_our_mul.log --outfile=pres_fig.png --op1=kern_mul --op2=bitwise_mul --op=our_mul 
+python3 graph_precision_relative.py --bitwidth=${BITWIDTH} --baseop=our_mul --op1=kern_mul --infile1=pres_kern_mul_v_our_mul.log --op2=bitwise_mul --infile2=pres_bitwise_mul_v_our_mul.log --outfile=pres_fig.png
 printf " done.\n\n"
 
 
