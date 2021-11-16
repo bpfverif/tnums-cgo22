851e1edcb48a7c772cb0ed0b97ce7590f7446aea
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2021-11-16 16:57:05 -0600

    refactor precision-increasing-bitwidth

--- precision-increasing-bitwidth/precision_increasing_bitwidth.cpp
+++ precision-increasing-bitwidth/precision_increasing_bitwidth.cpp
@@ -1,5 +1,6 @@
 #include "conc.h"
 #include "tnum.h"
+#include "tnum_util.hpp" 
 #include <boost/multiprecision/cpp_int.hpp>
 #include <cfenv>
 #include <cmath>
@@ -9,40 +10,12 @@
 
 typedef struct tnum tnum_t;
 
-int bitvec_width = 8;          // default bitvec witdh
+int bitvec_width = 8; // default bitvec witdh
 std::vector<tnum_t> all_tnums; // all possible n-bit tnums
-
-namespace bmp = boost::multiprecision;
-
 u64 num_output_equal = 0;
 u64 num_output_unequal = 0;
 u64 num_kern_mul_better = 0;
 u64 num_our_mul_better = 0;
-
-std::vector<tnum_t> generate_all_tnums() {
-
-  std::vector<tnum_t> all_tnums;
-  for (u64 i = 0; i < std::pow(2, bitvec_width); i++) {
-    for (u64 j = 0; j < std::pow(2, bitvec_width); j++) {
-      if ((i & j) == 0) {
-        tnum_t t = {.value = i, .mask = j};
-        all_tnums.push_back(t);
-      }
-    }
-  }
-  return all_tnums;
-}
-
-u64 get_number_of_concrete_values_in_tnum(tnum_t t) {
-  int z = 0;
-  for (auto i = 0; i < bitvec_width; i++) {
-    if ((t.mask & 1ULL) == 1ULL) {
-      z++;
-    }
-    t.mask = t.mask >> 1;
-  }
-  return static_cast<u64>(bmp::pow(bmp::cpp_int(2), z));
-}
 
 void calc_precision_helper(tnum_t t1, tnum_t t2) {
   tnum_t abs_res_1 = kern_mul(t1, t2);
@@ -60,9 +33,9 @@ void calc_precision_helper(tnum_t t1, tnum_t t2) {
   if (diff) {
     num_output_unequal += 1;
     u64 num_conc_in_abs_res_1 =
-        get_number_of_concrete_values_in_tnum(abs_res_1);
+        get_number_of_concrete_values_in_tnum(abs_res_1, bitvec_width);
     u64 num_conc_in_abs_res_2 =
-        get_number_of_concrete_values_in_tnum(abs_res_2);
+        get_number_of_concrete_values_in_tnum(abs_res_2, bitvec_width);
 
     if (num_conc_in_abs_res_1 > num_conc_in_abs_res_2) {
       num_our_mul_better += 1;
@@ -99,7 +72,7 @@ int main(int argc, char **argv) {
   bitvec_width = atoi(argv[1]);
 
   // generate all n bit tnums
-  all_tnums = generate_all_tnums();
+  all_tnums = generate_all_tnums(bitvec_width);
   calc_precision();
 
   double num_tnum_pairs = std::pow(all_tnums.size(), 2);
@@ -125,30 +98,6 @@ int main(int argc, char **argv) {
   std::cout << num_our_mul_better;
   std::cout << " (" << perc_our_mul_better << "%)";
   std::cout << std::endl;
-
-  //   std::cout << std::left << std::setw(5) << std::setfill('*') <<
-  //   bitvec_width
-  //             << ", " << std::setw(10) << std::fixed << std::setprecision(0)
-  //             << num_tnum_pairs << ", " << std::setw(8) <<
-  //             std::setprecision(0)
-  //             << num_output_equal << "(" << std::setprecision(3) <<
-  //             perc_equal << "%)"
-  //             << ", " << std::setw(8) << std::setprecision(0) <<
-  //             num_output_unequal
-  //             << "(" << std::setprecision(3) << perc_unequal << "%)"
-  //             << ", " << num_our_mul_better << ", " << num_kern_mul_better <<
-  //             std::endl;
-
-  // std::cout << "number of input tnum pairs where our_mul had
-  // better precision: "
-  // 	<< num_kern_mul_better << std::endl;
-  // std::cout << "number of input tnum pairs where other_mul had
-  // better precision: "
-  //  	<< num_our_mul_better << std::endl;
-  // std::cout << "total number of input tnum pairs where output
-  // from our_mul != other_mul : "
-  // 	<< num_kern_mul_better + num_our_mul_better;
-  // std:cout << std::endl;
 
   return 0;
 }
